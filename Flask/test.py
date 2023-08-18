c47efaeb63d4064ce7ddea9ab0e7bd68687e7a3b
CallumDrake1996 <mrjoker122@hotmail.co.uk>
2023-08-18 22:44:40 +0100

    login page with flask

---
+++ Flask/test.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import MySQLdb.cursors, re, hashlib
+# Route for handling the login page logic
+
+
+
+app = Flask(__name__)
+
+
+# Change this to your secret key (it can be anything, it's for extra protection)
+app.secret_key = 'your secret key'
+
+# Enter your database connection details below
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'MakeItOrTakeIt'
+
+# Intialize MySQL
+mysql = MySQL(app)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    error = None
+    return render_template('HomePage.html', error = error)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # Output a message if something goes wrong...
+    msg = ''
+    # Check if "username" and "password" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+        # Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM Log WHERE username = %s AND password = %s', (username, password,))
+        # Fetch one record and return result
+        account = cursor.fetchone()
+        # If account exists in accounts table in out database
+        if account:
+            # Create session data, we can access this data in other routes
+            session['loggedin'] = True
+            session['username'] = account['username']
+            # Redirect to home page
+            return 'Logged in successfully!'
+        else:
+            # Account doesnt exist or username/password incorrect
+            msg = 'Incorrect username/password!'
+    # Show the login form with message (if any)
+    return render_template('LogIn.html', msg=msg)
+# debug for flask 
+if __name__ == '__main__':
+    app.run(debug=True)
