2fb66cde9452dc6d962da822fece8ba00687ffbf
Stephen Harkins <stephenjharkins@hotmail.co.uk>
2023-08-22 16:40:15 +0100

    tidy up and basic Flask rendeed

---
+++ flask_routes.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+# this file can be used to store .routes to all templates
+
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/recipes')
+def recipes():
+    return render_template('recipes.html')
+
+@app.route('/shop')
+def shop():
+    return render_template('shopping_cart.html')
+
+@app.route('/login')
+def login():
+    return render_template('LogIn.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
