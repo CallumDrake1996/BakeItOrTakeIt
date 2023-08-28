edcea44fdda4b569fe897b8c84d6fa0251292654
Stephen Harkins <stephenjharkins@hotmail.co.uk>
2023-08-28 21:13:01 +0100

    adjusting header, added space for welcome message

--- flask_routes.py
+++ flask_routes.py
@@ -120,5 +120,9 @@ def recipes():
 def shop():
     return render_template('shopping_cart.html')
 
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
