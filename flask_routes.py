439f41c6ef828fee1b77ab6b598106f9fde2f474
CallumDrake1996 <mrjoker122@hotmail.co.uk>
2023-09-10 21:26:10 +0100

    all recipie and cart feature and confirmation email

--- flask_routes.py
+++ flask_routes.py
@@ -7,6 +7,7 @@ import MySQLdb.cursors, re, hashlib
 import math
 from flask import Flask,render_template,request,redirect,flash
 from flask import jsonify
+import random
 
 
 
@@ -15,27 +16,36 @@ log = 0
 global total_price
 global username 
 global total_items
-username = 'no'
+global orderNum
+global name
+global idNum
+global password
+password = ''
+orderNum = random.randint(100000,999999)
+username = 'NA'
 total_price = 0
 total_items = 0
+name = 'NA'
+name2 = 'NA'
+idNum = 0
 
 
 
 cart_contents = []
 products = [
-    {"id": 1, "name": "Bakewell tart", "price": 11.50, 'image': '/static/recipe_img/Bakewell.jpeg'},
-    {"id": 2, "name": "Banana Bread", "price": 12.03, 'image': '/static/recipe_img/Bananabread.jpeg'},
-    {"id": 3, "name": "Belgian buns", "price": 21.24, 'image': '/static/recipe_img/bellgianbread.jpeg'},
-    {"id": 4, "name": "Chocolate Brownies", "price": 20.60, 'image': '/static/recipe_img/chocolatebrownies.jpeg'},
-    {"id": 5, "name": "Chocolate Chip Cookies", "price": 10.20, 'image': '/static/recipe_img/chocochipcookies.jpeg'},
-    {"id": 6, "name": "Cinnamon rolls", "price": 20.33, 'image': '/static/recipe_img/fudgybrownies.jpeg'},
-    {"id": 7, "name": "Fudgy brownies", "price": 22.71, 'image': '/static/recipe_img/fudgybrownies.jpeg'},
-    {"id": 8, "name": "Iced buns with cream & jam", "price": 18.99, 'image': '/static/recipe_img/icedbunswithcreamandjam.jpeg'},
-    {"id": 9, "name": "lemon baked cheesecake", "price": 4.20, 'image': '/static/recipe_img/lemonbakedcheesecake.jpeg'},
-    {"id": 10, "name": "Red velvet cupcakes", "price": 19.65, 'image': '/static/recipe_img/Red-Velvet-Cupcakes.jpeg'},
-    {"id": 11, "name": "Rhubarb & custard sandwich biscuits", "price": 14.0, 'image': '/static/recipe_img/RubarbandCustard.jpeg'},
-    {"id": 12, "name": "Triple choco peanut butter layer cake", "price": 12.20, 'image': '/static/recipe_img/triplechocolateandpeanutbutterlayercake.jpeg'},
-    {"id": 13, "name": "Tropical upside-down cake", "price": 12.40, 'image': '/static/recipe_img/tropical-upside-down-cake.jpeg'},]
+    {"id": 1, "name": "Bakewell tart", "price": 11.50, 'image': '/static/recipe_img/Bakewell.jpeg', 'html':'BakewellTart' },
+    {"id": 2, "name": "Banana Bread", "price": 12.03, 'image': '/static/recipe_img/Bananabread.jpeg','html':'BananaBread'},
+    {"id": 3, "name": "Belgian buns", "price": 21.24, 'image': '/static/recipe_img/bellgianbread.jpeg','html':'BelgianBuns'},
+    {"id": 4, "name": "Chocolate Brownies", "price": 20.60, 'image': '/static/recipe_img/chocolatebrownies.jpeg','html':'ChocolateBrownies'},
+    {"id": 5, "name": "Chocolate Chip Cookies", "price": 10.20, 'image': '/static/recipe_img/chocochipcookies.jpeg','html':'ChocolateChipCookies'},
+    {"id": 6, "name": "Cinnamon rolls", "price": 20.33, 'image': '/static/recipe_img/fudgybrownies.jpeg','html':'CinnamonRolls'},
+    {"id": 7, "name": "Fudgy brownies", "price": 22.71, 'image': '/static/recipe_img/fudgybrownies.jpeg','html':'FudgyBrownies'},
+    {"id": 8, "name": "Iced buns with cream & jam", "price": 18.99, 'image': '/static/recipe_img/icedbunswithcreamandjam.jpeg','html':'IcedBunsWithCreamAndJam'},
+    {"id": 9, "name": "lemon baked cheesecake", "price": 4.20, 'image': '/static/recipe_img/lemonbakedcheesecake.jpeg','html':'LemonBakedCheesecake'},
+    {"id": 10, "name": "Red velvet cupcakes", "price": 19.65, 'image': '/static/recipe_img/Red-Velvet-Cupcakes.jpeg','html':'RedVelvetCupcakes'},
+    {"id": 11, "name": "Rhubarb & custard sandwich biscuits", "price": 14.0, 'image': '/static/recipe_img/RubarbandCustard.jpeg','html':'RhubarbAndCustardSandwichBiscuits'},
+    {"id": 12, "name": "Triple choco peanut butter layer cake", "price": 12.20, 'image': '/static/recipe_img/triplechocolateandpeanutbutterlayercake.jpeg','html':'TripleChocolateAndPeanutButterLayerCake'},
+    {"id": 13, "name": "Tropical upside-down cake", "price": 12.40, 'image': '/static/recipe_img/tropical-upside-down-cake.jpeg','html':'TropicalUpsideDownCake'}]
 
 def get_total_items(cart):
     global total_items
@@ -62,7 +72,7 @@ app.secret_key = 'work'
 # Enter your database connection details below
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_PASSWORD'] = 'PASSWORD'
 app.config['MYSQL_DB'] = 'MakeItOrTakeIt'
 
 # config mail
@@ -119,6 +129,8 @@ def logout():
     global total_items
     global cart_contents
     global log
+    global idNum
+    global client
     log = 0
     username = 'no'
     total_price = 0
@@ -127,6 +139,8 @@ def logout():
     session["loggedin"] = False
     session["name"] = None
     session['cart'] = []
+    idNum = 0
+    password= ''
     return redirect("/")
 
 
@@ -134,11 +148,22 @@ def logout():
 def home():
     msg=''
     global username
+    global name
+    global name2
+    global idNum
+    global password
     cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
     cursor.execute('SELECT first_name FROM Login WHERE username = %s', (username,))
     first_name = cursor.fetchone()
-    username = first_name
-    return render_template('home.html', msg = msg ,username = username, total_items=total_items)
+    name = first_name
+    cursor.execute('SELECT last_name FROM Login WHERE username = %s', (username,))
+    last_name = cursor.fetchone()
+    name2= last_name
+    cursor.execute('SELECT User_id FROM Login WHERE username = %s', (username,))
+    idNum = cursor.fetchone()
+    cursor.execute('SELECT password FROM Login WHERE username = %s', (username,))
+    password = cursor.fetchone()
+    return render_template('home.html', msg = msg ,name = name, total_items=total_items)
 
 @app.route('/SignUp', methods=['GET', 'POST'])
 def SignUp():
@@ -196,21 +221,52 @@ def contact_us():
         contact = request.form['message']
         msg = 'Email sent! Thank you we will be right on it :)'
         text_body = render_template('email_contact_us.html',email = email, message= contact)
-        text_body2 = render_template('email_contact_response.html',email = email, message= contact)
         text = Message(subject='contact us', html=text_body, sender =('BakeItOrTakeIt', 't43797192@gmail.com'), recipients = ['t43797192@gmail.com'])
-        text2 = Message(subject='contact us', html=text_body2, sender =('BakeItOrTakeIt', 't43797192@gmail.com'), recipients = [email])
         mail.send(text)
-        mail.send(text2)
-    return render_template('contact_us.html', msg = msg, username = username, total_items=total_items)
+    return render_template('contact_us.html', msg = msg, name = name, total_items=total_items)
 
+@app.route('/cart/checkout', methods=['GET', 'POST'])
+def checkout():
+    global orderNum
+    global cart_contents
+    global products
+    global username
+    total_price = calculate_total_price(cart_contents)
+    total_items = get_total_items(cart_contents)
+    basket_amount = total_price
+    basket_amount_delivery= float(basket_amount + 2.50)
+    format(basket_amount_delivery, '.2f')
+    firstname = request.form['Firstname']
+    lastname = request.form['Lastname']
+    streetname = request.form['StreetName']
+    postcode = request.form['Postcode']
+    city = request.form['City']
+    order = session['cart']
+    if request.method == 'POST' and 'CardNum' in request.form:
+        orderNum = random.randint(100000, 999999)
+        text_body = render_template('email_checkout.html', amount = basket_amount_delivery, firstname=firstname, lastname=lastname, streetname=streetname, postcode=postcode,  city=city, orderNum= orderNum, order=order)
+        text = Message(subject='Order Confirmation', html=text_body, sender =('BakeItOrTakeIt', 't43797192@gmail.com'), recipients = [username])
+        mail.send(text)
+        clear_cart()
+        session['cart'] = []
+        session.modified = True
+    return render_template('checkout.html', name= name, total_price = format(basket_amount, '.2f'), total_price_delivery =  format(basket_amount_delivery, '.2f'), total_items=total_items, firstname=firstname, lastname=lastname, streetname=streetname, postcode=postcode,  city=city, orderNum= orderNum)
+
+# @app.errorhandler(404)
+# def page_not_found(e):
+#     return render_template('404.html'), 404
+
+@app.route('/myprofile')
+def myProfile():
+    return render_template('MyProfile.html', name = name, name2 =name2, username= username, idNum = idNum, password =password)
 
 @app.route('/about')
 def about():
-    return render_template('about.html', username = username, total_items=total_items)
+    return render_template('about.html', name = name, total_items=total_items)
 
 @app.route('/recipes')
 def recipes():
-    return render_template('Recipes.html', username = username, total_items=total_items)
+    return render_template('RecipeLayout.html', name = name, total_items=total_items)
 
 @app.route('/shop', methods=['GET', 'POST'])
 def shop():
@@ -222,7 +278,7 @@ def shop():
         out1 = True
     else:
         in1 = True
-    return render_template('shop.html', username= username, total_items=total_items, products=products, in1 =in1, out1=out1)
+    return render_template('shop.html', name = name, total_items=total_items, products=products, in1 =in1, out1=out1)
 
 @app.route('/cart')
 def cart():
@@ -234,7 +290,7 @@ def cart():
     basket_amount = total_price
     basket_amount_delivery= float(basket_amount + 2.50)
     format(basket_amount_delivery, '.2f')
-    return render_template('shopping_cart.html', username= username, total_price = format(basket_amount, '.2f'), total_price_delivery =  format(basket_amount_delivery, '.2f'), total_items=total_items, msg= msg,)
+    return render_template('shopping_cart.html', name= name, total_price = format(basket_amount, '.2f'), total_price_delivery =  format(basket_amount_delivery, '.2f'), total_items=total_items, msg= msg,)
 
 @app.route('/totalitems')
 def totalitems():
@@ -287,8 +343,62 @@ def update_cart():
 
         # Update the quantity in the cart
         item['quantity'] = new_quantity
-
     return redirect(url_for('cart'))
+
+@app.route('/BakewellTart')
+def BakewellTart():
+    return render_template('BakewellTart.html', name = name, total_items=total_items)
+
+@app.route('/BananaBread')
+def BananaBread():
+    return render_template('BananaBread.html', name = name, total_items=total_items)
+
+@app.route('/BelgianBuns')
+def BelgianBuns():
+    return render_template('BelgianBuns.html', name = name, total_items=total_items)
+
+@app.route('/ChocolateBrownies')
+def ChocolateBrownies():
+    return render_template('ChocolateBrownies.html', name = name, total_items=total_items)
+
+@app.route('/ChocolateChipCookies')
+def ChocolateChipCookies():
+    return render_template('ChocolateChipCookies.html', name = name, total_items=total_items)
+
+@app.route('/CinnamonRolls')
+def CinnamonRolls():
+    return render_template('CinnamonRolls.html', name = name, total_items=total_items)
+
+@app.route('/FudgyBrownies')
+def FudgyBrownies():
+    return render_template('FudgyBrownies.html', name = name, total_items=total_items)
+
+@app.route('/IcedBunsWithCreamAndJam')
+def IcedBunsWithCreamAndJam():
+    return render_template('IcedBunsWithCreamAndJam.html', name = name, total_items=total_items)
+
+@app.route('/LemonBakedCheesecake')
+def LemonBakedCheesecake():
+    return render_template('LemonBakedCheesecake.html', name = name, total_items=total_items)
+
+@app.route('/RedVelvetCupcakes')
+def RedVelvetCupcakes():
+    return render_template('RedVelvetCupcakes.html', name = name, total_items=total_items)
+
+@app.route('/RhubarbAndCustardSandwichBiscuits')
+def RhubarbAndCustardSandwichBiscuits():
+    return render_template('RhubarbAndCustardSandwichBiscuits.html', name = name, total_items=total_items)
+
+@app.route('/TripleChocolateAndPeanutButterLayerCake')
+def TripleChocolateAndPeanutButterLayerCake():
+    return render_template('TripleChocolateAndPeanutButterLayerCake.html', name = name, total_items=total_items)
+
+@app.route('/TropicalUpsideDownCake')
+def TropicalUpsideDownCake():
+    return render_template('TropicalUpsideDownCake.html', name = name, total_items=total_items)
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
